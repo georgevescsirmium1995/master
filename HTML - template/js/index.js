2dcf4b42726049e12a548a3ab20f42939e383505
Dusan Jovicic <dusanjovicic@privacyone.io>
2021-10-13 00:04:08 +0200

    desktop version restyle

--- HTML - template/js/index.js
+++ HTML - template/js/index.js
@@ -151,9 +151,3 @@ function confirmation( question, title, confirm, cancel) {
     } );
     return defer.promise();
 }
-var toggler = document.getElementById("toggle-container");
-var toggleContents = document.getElementById("toggle-content");
-document.addEventListener("click", function(){
-//Apply a class intance to each navigation and set display to toggle: 
-     toggleContents.classList.toggle("displayed");
-});
